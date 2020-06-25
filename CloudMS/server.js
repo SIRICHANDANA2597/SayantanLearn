b3c0337f41a9f13ba6fdb87cb441edf54ce12b21
IKEAPFU <37018828+SIRICHANDANA2597@users.noreply.github.com>
2020-06-25 16:52:27 +0530

    Update server.js

--- CloudMS/server.js
+++ CloudMS/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8090;
+const port = process.env.PORT || 8080;
 
 const routes = require('./api/routes');
 routes(app);
