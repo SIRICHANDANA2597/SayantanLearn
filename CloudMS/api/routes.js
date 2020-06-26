7f91f14f7bc65862be12177534dc0948600ba59c
IKEAPFU <37018828+SIRICHANDANA2597@users.noreply.github.com>
2020-06-26 16:39:51 +0530

    Update routes.js

--- CloudMS/api/routes.js
+++ CloudMS/api/routes.js
@@ -5,4 +5,6 @@ console.log(3);
 module.exports = function(app) {
    app.route('/products')
        .get(controller.getProducts);
+   app.route('/hotels/:slno?')
+       .get(controller.gethotels);
 };
