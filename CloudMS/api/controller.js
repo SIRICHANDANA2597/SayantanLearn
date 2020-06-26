514b210be3ac056ccb954b4f743715b6f2817ab1
IKEAPFU <37018828+SIRICHANDANA2597@users.noreply.github.com>
2020-06-26 16:42:31 +0530

    Update controller.js

--- CloudMS/api/controller.js
+++ CloudMS/api/controller.js
@@ -2,6 +2,7 @@
 
 var properties = require('../package.json');
 var products = require('../service/Product');
+var hotels = require('../service/Hotels');
 console.log(4);
 var controllers = {
    getProducts: function(req, res) {
@@ -11,6 +12,15 @@ var controllers = {
                res.json(productlist);
            });
        },
+   gethotels: function(req, res) {
+        var new_var = req.params.slno;
+        //console.log(new_var);
+        hotels.find(req, res, new_var, function(err, hotellist) {
+                    if (err)
+                        res.send(err);
+                    res.json(hotellist);
+                });
+            },
 };
 
 module.exports = controllers;
